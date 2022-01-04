ebf34a6f8b24ac9368eec35ab14449d821a524fc
Olek <karagolek@gmail.com>
2022-01-04 03:55:18 +0100

    koncowa wersja zadania

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
-app.get("/", function (req, res) {
+app.get("/date", function (req, res) {
     res.send("<h1>Aleksander Karagiannakos 3i2 </h1>")
 })
 
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
+})
+app.get("/", function (req, res) {
+    res.send("<h1>zadanie domowe aplikacje klienckie </h1>")
 })
