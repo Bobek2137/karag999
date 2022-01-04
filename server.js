9ca6679523d904494fefaabedc76918d6999c733
Olek <karagolek@gmail.com>
2022-01-04 08:18:24 +0100

    teraz jest dobrze

--- server.js
+++ server.js
@@ -3,20 +3,22 @@ const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/data", function (req, res) {
-    res.send("<h1>Imie: ALeksander, nazwisko: Karagiannakos , klasa 3i2, grupa 1</h1>")
+    res.send(dane)
 })
 
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
 })
 app.get("/", function (req, res) {
-    res.send(dane)
+    res.send("<h1>zadanie domowe aplikacje klienckie</h1>")
 })
 
 const dane = {
     imie: "Aleksander",
     nazwisko: "karagiannakos",
     klasa: "3i2",
-    grupa: "1",
+    grupa: "1"
+
+
 
 }
