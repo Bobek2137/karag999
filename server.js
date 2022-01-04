e0831db00f969376e1725eb66fc64073516c7539
Olek <karagolek@gmail.com>
2022-01-04 03:57:27 +0100

    koncowa wersja

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
-app.get("/date", function (req, res) {
+app.get("/data", function (req, res) {
     res.send("<h1>Aleksander Karagiannakos 3i2 </h1>")
 })
 
